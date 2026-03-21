e1f912eaf1074cfeb2cdce9a2ada9d67a8a4e782
riccardo-ottavi <ottavi.ps@libero.it>
2026-03-21 16:51:03 +0100

    scheletro struttura app

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,32 @@ import './App.css'
 function App() {
 
   return (
-    <div className="container">
-      <h1>Spotify</h1>
-    </div>
+    <>
+      <section className="header">
+        <header>
+          {/* header*/}
+          <h1>Spotify</h1>
+        </header>
+      </section>
+
+      <section className="middle-big-box">
+        <div className="side-left">
+          {/* left-side-menu*/}
+        </div>
+        <div className="main-container">
+          {/* main*/}
+        </div>
+        <div className="side-right">
+          {/* left-side-menu*/}
+        </div>
+      </section>
+
+      <section className="footer">
+        <footer>
+          {/* footer*/}
+        </footer>
+      </section>
+    </>
   )
 }
 

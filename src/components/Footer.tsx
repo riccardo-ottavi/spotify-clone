6033e3ae9a1b04f14141a14b82e34c16cfcaeead
riccardo-ottavi <ottavi.ps@libero.it>
2026-03-23 12:39:57 +0100

    inizializza le icone del footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,10 @@ export default function Footer(){
         <footer>
             <h2>Sono il footer</h2>
             <img src="../shuffle-solid-full.svg" alt="" />
-            <img src="../icons.svg" alt="" />
+            <img src="../backward-solid-full.svg" alt="" />
+            <img src="../circle-play-solid-full.svg" alt="" />
+            <img src="../forward-solid-full.svg" alt="" />
+            <img src="../repeat-solid-full.svg" alt="" />
         </footer>
     )
 }

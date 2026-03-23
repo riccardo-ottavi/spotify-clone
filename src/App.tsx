6212e7dfb6941cd53aee8a509d88458b393c546b
riccardo-ottavi <ottavi.ps@libero.it>
2026-03-23 11:58:30 +0100

    inserisci elementi parte centrale

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,24 @@ function App() {
           <h3>Sono la parte sinistra</h3>
         </div>
         <div className="big-mid">
-          Sono la parte centrale 
+          <nav>
+            <a href="">Tutto</a>
+            <a href="">Musica</a>
+            <a href="">Podcast</a>
+          </nav>
+
+        <div className="orizzontal-cards-container">
+          <div className="orizzontal-card">TItolo</div>
+          <div className="orizzontal-card">TItolo</div>
+          <div className="orizzontal-card">TItolo</div>
+          <div className="orizzontal-card">TItolo</div>
+          <div className="orizzontal-card">TItolo</div>
+          <div className="orizzontal-card">TItolo</div>
+          <div className="orizzontal-card">TItolo</div>
+          <div className="orizzontal-card">TItolo</div>
+        </div>
+
+
         </div>
         <div className="right-mid">
           <h3>Sono la parte destra</h3>
